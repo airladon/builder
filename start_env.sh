c80aac37efd10fbfdedb3a7f5457e0bbdefbf19e
airladon <airladon@gmail.com>
2019-10-11 15:48:54 -0700

    fix bug

--- start_env.sh
+++ start_env.sh
@@ -151,7 +151,7 @@ elif [ $1 = 'prod' ];
     -v $PROJECT_PATH/repo:/opt/app/repo \
     -v /var/run/docker.sock:/var/run/docker.sock \
     -e HOST_PATH=$PROJECT_PATH/repo/clone \
-    --restart $DOCKER_RESTART
+    --restart $DOCKER_RESTART \
     -d \
     builder-$1
 else
