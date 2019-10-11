c62a7eec2adbcb50aa8c136136a01995b92384da
airladon <airladon@gmail.com>
2019-10-11 14:48:09 -0700

    add debug lines

--- start_env.sh
+++ start_env.sh
@@ -104,14 +104,16 @@ echo "${bold}${cyan}================= Building container ===================${re
 cp containers/$DOCKERFILE DockerfileTemp
 # set user id of new user in production container to the same user id of the 
 # user calling the container so permissions of files work out ok
+echo 1
 DOCKER_GROUP_ID=`grep docker /etc/group | sed 's/[^:]*:[^:]*:\([0-9]*\).*/\1/'`
 if [ -z "$DOCKER_GROUP_ID" ];
 then
+  echo 2
   DOCKER_GROUP_ID=`ls -n /var/run/docker.sock | sed "s/[^ ]* *[^ ]* *\([^ ]*\).*/\1/"`
 fi
 HOST_USER_GROUP_ID=`id -g`
 HOST_USER_ID=`id -u`
-
+echo 3
 cat DockerfileTemp | sed "s/HOST_USER_ID/${HOST_USER_ID}/" | sed "s/HOST_USER_GROUP_ID/${HOST_USER_GROUP_ID}/" | sed "s/DOCKER_GROUP_ID/${DOCKER_GROUP_ID}/" > Dockerfile
 rm DockerfileTemp
 
